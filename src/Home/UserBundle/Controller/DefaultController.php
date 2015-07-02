928d90a8c8c8e765ccd2bcd8e77374da6fad58a4
Stephan TAPE <tape.stephan@gmail.com>
2015-07-02 13:16:16 +0200

    up

--- src/Home/UserBundle/Controller/DefaultController.php
+++ src/Home/UserBundle/Controller/DefaultController.php
@@ -42,25 +42,21 @@ class DefaultController extends Controller
      */
     public function homeAction()
     {
-    	$em = $this->getDoctrine()->getManager();
     	// A la connexion
         if($_POST) {
             if (($_POST['email'] != "") && ($_POST['password'] != "")) {
-            	$userMail = $_POST['email'];
-            	$userPassword = $_POST['password'];
-            	$user = $em->getRepository('UserBundle:User')->findOneByEmail($userMail);
-            	if($user){	// Utilisateur trouvé
-	            	if($user->getPassword() == $userPassword){
-	            		// Connecter l'user: creer une fonction pour tout foutre en session, etc. demain quoi...
-	            		die('ok');
-	                	return  $this->render("UserBundle:Default:home.html.twig");
-	            	}
-	                else
-	                	die( 'MAUVAIS PASSWORD CONNARD');
+            	$em = $this->getDoctrine()->getManager();
+            	$user = $em->getRepository('UserBundle:User')->findOneByEmail($_POST['email']);
+            	if($user && $user->getPassword() == $_POST['password']){
+            		// Mettre en Session
+            		$this->getRequest()->getSession()->set('user', $user);
+            		// Rediriger vers home connecté
+                	return  $this->render("UserBundle:Default:home.html.twig");
             	}
-                else
-                	die('AUCUN USER. INSCRIS TOI BATARD');
-                	// return $this->render('UserBundle:Default:inscription.html.twig');
+                else{
+                	// Message d'erreur
+                	die( 'MAUVAIS PASSWORD');
+                }
             }
         }
         return array();
@@ -74,7 +70,6 @@ class DefaultController extends Controller
     {
     	if($_POST){
     		$em = $this->getDoctrine()->getManager();
-    		var_dump($_POST);
     		$newUser = new User();
     		$newUser->setName($_POST['nom']);
     		$newUser->setFirstName($_POST['prenom']);
@@ -82,9 +77,27 @@ class DefaultController extends Controller
     		$newUser->setPassword($_POST['password']);
     		$em->persist($newUser);
     		$em->flush();
-    		$data = array('success' => 'Utilisateur inséré!');
-    		return $this->render('UserBundle:Default:inscription.html.twig');
+
+    		// Ajout en session
+    		$this->getRequest()->getSession()->set('user', $newUser);
+
+    		// Redirection vers home connecté
+    		return $this->redirect('./home');
     	}
     	return $this->render('UserBundle:Default:inscription.html.twig');
+    }
+
+    /**
+     * @Route("/nos-offres")
+     * @Template()
+     */
+    public function offresAction()
+    {
+		// Check session
+		if(!$this->getRequest()->getSession()->get('user')){
+			die("Connecte-toi, mec!");
+		}
+
+    	return $this->render('UserBundle:Default:offres.html.twig');
     }
 }
